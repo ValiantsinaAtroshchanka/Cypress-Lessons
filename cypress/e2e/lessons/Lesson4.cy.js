fc3a1d922d786ce45abc3ac0574b3efc82f7b256
Valiantsina Atroschenko <valiantsina.atroshchanka@gmail.com>
2022-06-23 17:39:53 +0300

    delete 3 and4

--- cypress/e2e/lessons/Lesson4.cy.js
+++
@@ -1,9 +0,0 @@
-it('',()=>{
-    cy.visit('https://coursehunter.net/');
-    cy.get('input[type="search"]')
-    .type(100)
-    // задали значение элементу
-    .should('have.value'100)
-    .and('be.visible')
-}
-)
